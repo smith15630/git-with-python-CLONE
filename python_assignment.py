9c7387df7a895efb92972fee0e228f9342292270
smith15630 <smith.15630@buckeyemail.osu.edu>
2022-07-07 01:42:01 +0000

    python assignment the sequel

---
+++ python_assignment.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# importing the dataset
+import sys
+import pandas
+df = pandas.read_csv(sys.argv[0])
+print(df)
+
+
+
+# In[10]:
+
+
+#scatterplot of data
+import matplotlib.pyplot as plt
+plt.scatter(df[['x']], df[['y']], color = 'red')
+plt.title('y vs x')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+plt.savefig('scatterplot')
+
+# In[20]:
+
+
+#scatterplot of data with line
+
+
+#calculate slope and intercept of regression line
+def abline(slope, intercept):
+    axes = plt.gca()
+    x_vals = np.array(axes.get_xlim())
+    y_vals = intercept + slope * x_vals
+    plt.plot(x_vals, y_vals, '--')
+slope = np.polyfit(df.x, df.y,1)[0]
+intercept = np.polyfit(df.x, df.y,1)[1]
+import matplotlib.pyplot as plt
+import numpy as np
+plt.scatter(df[['x']], df[['y']], color = 'red')
+plt.title('y vs x')
+plt.xlabel('x')
+abline(slope, intercept)
+plt.ylabel('y')
+plt.show()
+
+
+
